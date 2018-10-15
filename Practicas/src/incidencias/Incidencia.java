13d7aff2333a616955aaa79caeee8eb290ba3efc
Eduardo Garcia <Eduardo Garcia@DESKTOP-CSEB2MF>
2018-10-16 00:00:15 +0200

    20181015 trabajando en incidencias.
    Creacion de botones que crean e imprimimen incidencias en consola.

--- Practicas/src/incidencias/Incidencia.java
+++ Practicas/src/incidencias/Incidencia.java
@@ -15,6 +15,10 @@ public class Incidencia {
 		
 		this.descripcion = descripcion;
 		
+		NumeroIncidencia = contadorIncidencias;  //Chapucilla para crear un ID único y también para recorrer la array.
+		
+		contadorIncidencias++;
+		
 	}
 	
 	public String getDatosGenerales() {
@@ -23,10 +27,22 @@ public class Incidencia {
 				"\nDescripcion: "+this.descripcion;
 		
 	}
+	
+	public int getNumeroIncidencia() {
+		
+		return NumeroIncidencia;
+		
+	}
 
 	public String getFecha() {
 		
 		return dtf.format(fecha);
+		
+	}
+	
+	public static int getcontadorIncidencias() {
+		
+		return contadorIncidencias;
 		
 	}
 
@@ -38,6 +54,10 @@ public class Incidencia {
 	
 	private String cliente;
 	
+	private int NumeroIncidencia;
+	
+	private static int contadorIncidencias = 0;  //Esta variable la creo porque me sirve para recorrer la array de incidencias después. MEJORABLE.
+	
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
+	
 }
--- Practicas/src/incidencias/UsoIncidencia.java
+++ Practicas/src/incidencias/UsoIncidencia.java
@@ -1,7 +1,9 @@
 package incidencias;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 
 public class UsoIncidencia {
 
@@ -14,11 +16,6 @@ public class UsoIncidencia {
 		mimarco.setVisible(true);
 		
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Incidencia primera = new Incidencia("FEDEFARMA", 3233234, "Esto es una prueba");
-		//Incidencia segunda = new Incidencia("BOBOLI", 3233677, "Esto es otra prueba");
-		//System.out.println(primera.getDatosGenerales());
-		//System.out.println(segunda.getDatosGenerales());
 
 	}
 
@@ -31,7 +28,65 @@ class MarcoIncidencias extends JFrame{
 		setTitle("Gestion incidencias");
 		
 		setBounds(200,200,1200,800);
+		
+		LaminaIncidencias LaminaIncidencias = new LaminaIncidencias();
+		
+		add(LaminaIncidencias);
 			
+	}
+	
+}
+
+class LaminaIncidencias extends JPanel implements ActionListener{
+	
+	Incidencia [] misIncidencias=new Incidencia [100]; //No tengo claro porque he tenido que crear aquí la array, pero funciona
+	
+	JButton BotonNuevaIncidencia = new JButton("Crear incidencia");
+	
+	JButton BotonImprimirIncidencia = new JButton("Mostrar incidencia");
+	
+	public LaminaIncidencias () {
+		
+		add(BotonNuevaIncidencia);
+		
+		add(BotonImprimirIncidencia);
+		
+		BotonNuevaIncidencia.addActionListener(this);
+		
+		BotonImprimirIncidencia.addActionListener(this);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		Object botonPulsado = e.getSource();
+			
+		if (botonPulsado.equals(BotonNuevaIncidencia)) {
+			
+			String cliente=JOptionPane.showInputDialog("Introduce el cliente");
+			
+			int iet=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de iet"));
+			
+			String descripcion=JOptionPane.showInputDialog("Introduce la descripcion de la incidencia");
+		
+			misIncidencias[Incidencia.getcontadorIncidencias()] = new Incidencia(cliente, iet, descripcion);
+			
+		}
+		
+		if (botonPulsado.equals(BotonImprimirIncidencia)) {
+			
+			for(int i = 0; i < misIncidencias.length; i++){
+				
+				if(i<Incidencia.getcontadorIncidencias()){
+					
+					System.out.println(misIncidencias[i].getDatosGenerales());
+					
+				}
+				
+			}
+		
+		}
+	
 	}
 	
 }
