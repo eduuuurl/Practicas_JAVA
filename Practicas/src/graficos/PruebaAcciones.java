695151f110290f45ca79d3602e680fdb8860b046
Edu <Edu@Edu-PC>
2018-10-24 23:14:24 +0200

    20181024 videos 78 y 79

--- Practicas/src/graficos/PruebaAcciones.java
+++ Practicas/src/graficos/PruebaAcciones.java
@@ -48,11 +48,16 @@ class PanelAction extends JPanel{
 		AccionColor accionazul = new AccionColor("azul", 
 				new ImageIcon("src/graficos/azul.gif"),new Color(150,203,218));
 		
-		JButton botonazul = new JButton("Azul",new ImageIcon("src/graficos/azul.gif"));
+		/*Los botones son los objetos fuente del evento, el boton en si
+		 * al ser pulsado lanza un evento. Usamos un constructor de JButton
+		 * al que se le pasa como parametro una accion, y de esa accion pilla
+		 * sus datos*/
 		
-		JButton botonrojo = new JButton("Rojo",new ImageIcon("src/graficos/rojo.gif"));
+		JButton botonamarillo = new JButton(accionamarillo);
 		
-		JButton botonamarillo = new JButton("Amarillo",new ImageIcon("src/graficos/amarillo.gif"));
+		JButton botonrojo = new JButton(accionrojo);
+		
+		JButton botonazul = new JButton(accionazul);
 		
 		add(botonazul);
 		
@@ -60,33 +65,70 @@ class PanelAction extends JPanel{
 		
 		add(botonamarillo);
 		
+		//Esto es para crear una segunda fuente de evento, combinacion de teclas
+		//Primero necesitamos crear un InputMap
+		
+		InputMap mapaEntrada = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		
+		//Ahora creamos la combinacion de teclas
+		
+		KeyStroke teclaAmarillo = KeyStroke.getKeyStroke("ctrl Y");
+		
+		KeyStroke teclaAzul = KeyStroke.getKeyStroke("ctrl B");
+		
+		KeyStroke teclaRojo = KeyStroke.getKeyStroke("ctrl R");
+		
+		//Asignar la combinacion de teclas a un objeto
+		
+		mapaEntrada.put(teclaAmarillo, "fondo_amarillo");
+		
+		mapaEntrada.put(teclaAzul, "fondo_azul");
+		
+		mapaEntrada.put(teclaRojo, "fondo_rojo");
+		
+		ActionMap mapaAccion = getActionMap();
+		
+		mapaAccion.put("fondo_amarillo", accionamarillo);
+		
+		mapaAccion.put("fondo_azul", accionazul);
+		
+		mapaAccion.put("fondo_rojo", accionrojo);
+		
+	}
+	
+	// Esta va a ser la clase oyente, y va a ser una inner class
+
+	private class AccionColor extends AbstractAction{
+		
+		public AccionColor (String nombre, Icon icono, Color color_boton) {
+			
+			putValue(Action.NAME, nombre);
+			
+			putValue(Action.SMALL_ICON, icono);
+			
+			putValue(Action.SHORT_DESCRIPTION, "Poner la lamina de color "+ nombre);
+			
+			putValue("color_de_fondo", color_boton);
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			Color c = (Color)getValue("color_de_fondo");
+			
+			setBackground(c);
+			
+			System.out.println("Nombre de boton pulsado: "+getValue(Action.NAME));
+			
+			System.out.println("Dscripción: "+getValue(Action.SHORT_DESCRIPTION));
+			
+		}
+		
 	}
 	
 }
 
-// Esta va a ser la clase oyente
 
-class AccionColor extends AbstractAction{
-	
-	public AccionColor (String nombre, Icon icono, Color color_boton) {
-		
-		putValue(Action.NAME, nombre);
-		
-		putValue(Action.SMALL_ICON, icono);
-		
-		putValue(Action.SHORT_DESCRIPTION, "Poner la lamina de color "+ nombre);
-		
-		putValue("color_de_fondo", color_boton);
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-}
 
