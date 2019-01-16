2877769e37bba64f57d784bd6a9f5b2b034e1830
Eduardo Garcia <Eduardo Garcia@192.168.1.144>
2019-01-16 23:44:31 +0100

    20190116 video 94 final

--- Practicas/src/graficos/Ejemplo_radio.java
+++ Practicas/src/graficos/Ejemplo_radio.java
@@ -34,7 +34,20 @@ class Lamina_radio extends JPanel{
 		
 		setLayout(new BorderLayout());
 		texto = new JLabel("En un lugar de la mancha de cuyo nombre...");
+		/*START VERSION NUEVA*/
+		texto.setFont(new Font("Serif",Font.PLAIN,12));
+		/*END VERSION NUEVA*/
 		add(texto, BorderLayout.CENTER);
+		lamina_botones = new JPanel();
+		migrupo = new ButtonGroup();
+		/*START VERSION NUEVA*/
+		colocarBotones("Pequeño", false, 10);
+		colocarBotones("Mediano", true, 14);
+		colocarBotones("Grande", false, 18);
+		colocarBotones("Muy grande", false, 24);
+		add(lamina_botones,BorderLayout.SOUTH);
+		/*END VERSION NUEVA*/
+		/*START VERSION CUTRE
 		ButtonGroup migrupo = new ButtonGroup();
 		boton1 = new JRadioButton("Pequeño",false);
 		boton2 = new JRadioButton("Mediano",true);
@@ -55,29 +68,71 @@ class Lamina_radio extends JPanel{
 		lamina_radio.add(boton3);
 		lamina_radio.add(boton4);
 		add(lamina_radio,BorderLayout.SOUTH);
+		END VERSION CUTRE*/
 		
 	}
+	/*START VERSION NUEVA*/
+	public void colocarBotones (String nombre, boolean seleccionado, final int tamano) {
+		
+		JRadioButton boton = new JRadioButton(nombre,seleccionado);
+		
+		migrupo.add(boton);
+		
+		lamina_botones.add(boton);
+		
+		ActionListener mievento = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				texto.setFont(new Font("Serif",Font.PLAIN,tamano));
+				
+			}
+		};
+		
+		boton.addActionListener(mievento);
+		
+	}
+	/*END VERSION NUEVA*/
 	
+	/*START VERSION CUTRE
 	private class Evento_radio implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			
-			/*Vamos a usar el metodo getSource para saber que boton
-			 * es el que esta lanzando el evento e*/
+			//Vamos a usar el metodo getSource para saber que boton
+			 // es el que esta lanzando el evento e
 			
 			if (e.getSource()==boton1) {
 				
-				System.out.println("Ha sido el boton 1");
+				texto.setFont(new Font ("Serif",Font.PLAIN,10));
+				
+			}else if (e.getSource()==boton2) {
+				
+				texto.setFont(new Font ("Serif",Font.PLAIN,14));
+				
+			}else if(e.getSource()==boton3) {
+				
+				texto.setFont(new Font ("Serif",Font.PLAIN,18));
+				
+			}else if(e.getSource()==boton4) {
+				
+				texto.setFont(new Font ("Serif",Font.PLAIN,24));
 				
 			}
 			
 		}		
 		
 	}
+	END VERSION CUTRE*/
 	
 	private JLabel texto;
 	private JRadioButton boton1, boton2, boton3, boton4;
+	/*START VERSION NUEVA*/
+	private ButtonGroup migrupo;
+	private JPanel lamina_botones;
+	/*END VERSION NUEVA*/
 	
 }
