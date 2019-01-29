19089c0f29aba80fd6bb535fab91eeeb91784450
Edu <Edu@192.168.1.143>
2019-01-29 23:41:51 +0100

    20190129 incidencias.
    Creacion de la parte superior de la aplicacion.

--- Practicas/src/incidencias/UsoIncidencia.java
+++ Practicas/src/incidencias/UsoIncidencia.java
@@ -6,38 +6,93 @@ import java.awt.event.*;
 
 
 public class UsoIncidencia {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 		/*Creación del marco de la aplicacion*/
-		MarcoIncidencias mimarco = new MarcoIncidencias();
-		
-		mimarco.setVisible(true);
-		
+		MarcoIncidencias mimarco = new MarcoIncidencias();		
+		mimarco.setVisible(true);		
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 	}
-
 }
 
-class MarcoIncidencias extends JFrame{
+class MarcoIncidencias extends JFrame{	
+	public MarcoIncidencias () {		
+		setTitle("Gestion incidencias");		
+		setBounds(100,100,1200,800);
+		LaminaIncidencias milamina = new LaminaIncidencias();
+		add(milamina);
+	}	
+}
+
+/*Esta es la lamina general, la que ocupa todo el marco por decirlo así*/
+class LaminaIncidencias extends JPanel{
+	public LaminaIncidencias () {
+		setVisible(true);
+		setLayout(new BorderLayout());
+		LaminaSuperior milaminasuperior = new LaminaSuperior();
+		add(milaminasuperior, BorderLayout.NORTH);
+	}
+}
+
+/*Esta es la lamina superior, en la que tendre los cuadros para introducir valores,
+ * y tambien los botones de la aplicacion, que estaran a la derecha*/
+class LaminaSuperior extends JPanel{
+	Font fuentebotones = new Font ("Serif",Font.BOLD,24);
+	public LaminaSuperior() {
+		setBackground(Color.BLUE); //Esto es solo para ver la lamina, quitarlo al final.
+		setVisible(true);
+		setLayout(new BorderLayout());
+		
+		/*Creación de los 3 botones de la app*/
+		JButton botoncrear = new JButton("Crear");
+		JButton botonbuscar = new JButton("Buscar");
+		JButton botonopciones = new JButton("Opciones");
+		botoncrear.setFont(fuentebotones);
+		botonbuscar.setFont(fuentebotones);
+		botonopciones.setFont(fuentebotones);
+		
+		/*Creacion de una lamina a la derecha, para poner los botones en ella*/
+		JPanel laminabotones = new JPanel();
+		laminabotones.setLayout(new GridLayout(3, 1));
+		add(laminabotones,BorderLayout.EAST);
+		laminabotones.add(botoncrear);
+		laminabotones.add(botonbuscar);
+		laminabotones.add(botonopciones);
+		
+		/*Creacion de una lamina a la izquierda, donde el usuario podrá
+		 * introducir datos*/
+		JPanel laminaentrada = new JPanel();
+		//laminaentrada.setLayout(new GridLayout(3,2));
+		add(laminaentrada,BorderLayout.WEST);
+		laminaentrada.add(new LaminaEntradaDatos("IET ",10));
+		laminaentrada.add(new LaminaEntradaDatos("Fecha ",10));
+		laminaentrada.add(new LaminaEntradaDatos("Cliente ",20));
+		laminaentrada.add(new LaminaEntradaDatos("Descripción ",40));
+		
+	}
 	
-	public MarcoIncidencias () {
-		
-		setTitle("Gestion incidencias");
-		
-		setBounds(200,200,1200,800);
-		
-		LaminaIncidencias lamina_principal = new LaminaIncidencias();
-		
-		add(lamina_principal);
+	/*Esta lamina es para poder crear los campos más rapido en la lamina de la izquierda,
+	 * pongo titulo y cuadro para introducir los datos*/
+	private class LaminaEntradaDatos extends JPanel{
+		public LaminaEntradaDatos (String nombre_campo, int ancho_campo) {
+			setVisible(true);
+			setLayout(new BorderLayout());
+			JLabel nombre = new JLabel(nombre_campo);
+			JTextField campo = new JTextField(ancho_campo);
+			nombre.setFont(fuentebotones);
+			add(nombre,BorderLayout.WEST);
+			add(campo, BorderLayout.EAST);
 			
+		}
 	}
 	
 }
 
-class LaminaIncidencias extends JPanel implements ActionListener{
+class LaminaInferior extends JPanel{
+	
+}
+
+/*class LaminaIncidencias extends JPanel implements ActionListener{
 	
 	Incidencia [] misIncidencias=new Incidencia [100]; //No tengo claro porque he tenido que crear aquí la array, pero funciona
 	
@@ -89,4 +144,4 @@ class LaminaIncidencias extends JPanel implements ActionListener{
 	
 	}
 	
-}
+}*/
