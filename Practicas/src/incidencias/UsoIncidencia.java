c4190a58cb3c56e97ca0d7e30cfb98f3429e9369
egarcia <egarcia@saepc91.ssi-schaefer.local>
2019-02-02 17:18:29 +0100

    20190202 creacion layout general
    Creacion de las dos laminas principales de la aplicación.
    
    Creación del campo fecha.

--- Practicas/src/incidencias/UsoIncidencia.java
+++ Practicas/src/incidencias/UsoIncidencia.java
@@ -3,6 +3,8 @@ package incidencias;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class UsoIncidencia {
@@ -12,6 +14,7 @@ public class UsoIncidencia {
 		MarcoIncidencias mimarco = new MarcoIncidencias();		
 		mimarco.setVisible(true);		
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mimarco.setResizable(false);
 	}
 }
 
@@ -20,15 +23,21 @@ class MarcoIncidencias extends JFrame{
 		setTitle("Gestion incidencias");		
 		setBounds(70,70,1700,800);
 		LaminaIncidencias milamina = new LaminaIncidencias();
-		add(milamina);
+		add(milamina);		
 	}	
 }
 
 /*Esta es la lamina general, la que ocupa todo el marco por decirlo así*/
 class LaminaIncidencias extends JPanel{
 	public LaminaIncidencias () {
+		BoxLayout layoutgeneral = new BoxLayout(this,BoxLayout.Y_AXIS);
+		setLayout(layoutgeneral);
 		LaminaSuperior milaminasuperior = new LaminaSuperior();
+		milaminasuperior.setPreferredSize(new Dimension(1700, 0));
 		add(milaminasuperior);
+		LaminaInferior milaminainferior = new LaminaInferior();
+		milaminainferior.setPreferredSize(new Dimension(1700, 677));
+		add(milaminainferior);
 	}
 }
 
@@ -45,7 +54,7 @@ class LaminaSuperior extends JPanel{
 		add(laminaentrada);
 		
 		LaminaEntradaDatos entrada1 = new LaminaEntradaDatos("IET ",7);
-		LaminaEntradaDatos entrada2 = new LaminaEntradaDatos("Fecha ",7);
+		LaminaEntradaDatosFecha entrada2 = new LaminaEntradaDatosFecha();		
 		LaminaEntradaDatos entrada3 = new LaminaEntradaDatos("Cliente ",10);
 		LaminaEntradaDatos entrada4 = new LaminaEntradaDatos("Descripción ",40);
 		
@@ -58,9 +67,11 @@ class LaminaSuperior extends JPanel{
 		JButton botoncrear = new JButton("Crear");
 		JButton botonbuscar = new JButton("Buscar");
 		JButton botonopciones = new JButton("Opciones");
+		JButton botonlimpiar = new JButton("Limpiar");
 		botoncrear.setFont(fuentebotones);
 		botonbuscar.setFont(fuentebotones);
 		botonopciones.setFont(fuentebotones);
+		botonlimpiar.setFont(fuentebotones);
 		
 		/*Creacion de una lamina a la derecha, para poner los botones en ella*/
 		JPanel laminabotones = new JPanel();
@@ -68,14 +79,14 @@ class LaminaSuperior extends JPanel{
 		add(laminabotones);
 		laminabotones.add(botoncrear);
 		laminabotones.add(botonbuscar);
-		laminabotones.add(botonopciones);	
+		laminabotones.add(botonopciones);
+		laminabotones.add(botonlimpiar);
 	}
 	
 	/*Esta lamina es para poder crear los campos más rapido en la lamina de la izquierda,
 	 * pongo titulo y cuadro para introducir los datos*/
 	private class LaminaEntradaDatos extends JPanel{
 		public LaminaEntradaDatos (String nombre_campo, int ancho_campo) {
-			setVisible(true);
 			setLayout(new BorderLayout());
 			JLabel nombre = new JLabel(nombre_campo);
 			JTextField campo = new JTextField(ancho_campo);
@@ -86,10 +97,28 @@ class LaminaSuperior extends JPanel{
 		}
 	}
 	
+	/*Esta nueva lamina la he tenido que crear para poder añadir una lamina de entrada de 
+	 * datos en la que salga la fecha de hoy. Luego eso deberá poderse cambiar*/
+	private class LaminaEntradaDatosFecha extends JPanel{
+		public LaminaEntradaDatosFecha () {
+			setLayout(new BorderLayout());
+			JLabel nombre = new JLabel("Fecha ");
+			String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+			JTextField campo = new JTextField(date, 7);
+			campo.setFont(fuenteentrada);
+			nombre.setFont(fuentebotones);
+			campo.setHorizontalAlignment(JTextField.CENTER);
+			add(nombre,BorderLayout.WEST);
+			add(campo, BorderLayout.EAST);			
+		}
+	}
+	
 }
 
 class LaminaInferior extends JPanel{
-	
+	public LaminaInferior () {
+		setBackground(new Color(200,205,182));
+	}
 }
 
 /*class LaminaIncidencias extends JPanel implements ActionListener{
