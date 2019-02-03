38e53fca48da6b9d060c2dc2532da126fa803fb6
egarcia <egarcia@saepc91.ssi-schaefer.local>
2019-02-03 23:28:52 +0100

    20190203 funcionalidad agregar
    Ahora ya se crea un nuevo jpanel cada vez que aprieto CREAR

--- Practicas/src/incidencias/UsoIncidencia.java
+++ Practicas/src/incidencias/UsoIncidencia.java
@@ -32,145 +32,116 @@ class LaminaIncidencias extends JPanel{
 	public LaminaIncidencias () {
 		BoxLayout layoutgeneral = new BoxLayout(this,BoxLayout.Y_AXIS);
 		setLayout(layoutgeneral);
-		LaminaSuperior milaminasuperior = new LaminaSuperior();
+		milaminasuperior = new LaminaSuperior();
 		milaminasuperior.setPreferredSize(new Dimension(1700, 0));
 		add(milaminasuperior);
-		LaminaInferior milaminainferior = new LaminaInferior();
+		milaminainferior = new LaminaInferior();
 		milaminainferior.setPreferredSize(new Dimension(1700, 677));
 		add(milaminainferior);
 	}
-}
-
-/*Esta es la lamina superior, en la que tendre los cuadros para introducir valores,
- * y tambien los botones de la aplicacion, que estaran a la derecha*/
-class LaminaSuperior extends JPanel{
-	Font fuentebotones = new Font ("Serif",Font.BOLD,20);
-	Font fuenteentrada = new Font ("Serif",Font.PLAIN,18);
-	public LaminaSuperior() {
-		
-		/*Creacion de una lamina a la izquierda, donde el usuario podrá
-		 * introducir datos*/
-		JPanel laminaentrada = new JPanel();
-		add(laminaentrada);
-		
-		LaminaEntradaDatos entrada1 = new LaminaEntradaDatos("IET ",7);
-		LaminaEntradaDatosFecha entrada2 = new LaminaEntradaDatosFecha();		
-		LaminaEntradaDatos entrada3 = new LaminaEntradaDatos("Cliente ",10);
-		LaminaEntradaDatos entrada4 = new LaminaEntradaDatos("Descripción ",40);
-		
-		laminaentrada.add(entrada1);
-		laminaentrada.add(entrada2);
-		laminaentrada.add(entrada3);
-		laminaentrada.add(entrada4);
-		
-		/*Creación de los 3 botones de la app*/
-		JButton botoncrear = new JButton("Crear");
-		JButton botonbuscar = new JButton("Buscar");
-		JButton botonopciones = new JButton("Opciones");
-		JButton botonlimpiar = new JButton("Limpiar");
-		botoncrear.setFont(fuentebotones);
-		botonbuscar.setFont(fuentebotones);
-		botonopciones.setFont(fuentebotones);
-		botonlimpiar.setFont(fuentebotones);
-		
-		/*Creacion de una lamina a la derecha, para poner los botones en ella*/
-		JPanel laminabotones = new JPanel();
-		laminabotones.setLayout(new GridLayout(1, 3));
-		add(laminabotones);
-		laminabotones.add(botoncrear);
-		laminabotones.add(botonbuscar);
-		laminabotones.add(botonopciones);
-		laminabotones.add(botonlimpiar);
+	
+	LaminaSuperior milaminasuperior;
+	LaminaInferior milaminainferior;
+	
+	private class Oyente_Agregar implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			milaminainferior.add(new LaminaIncidenciaInferior());
+			milaminainferior.revalidate();
+			milaminainferior.repaint();			
+		}		
 	}
 	
-	/*Esta lamina es para poder crear los campos más rapido en la lamina de la izquierda,
-	 * pongo titulo y cuadro para introducir los datos*/
-	private class LaminaEntradaDatos extends JPanel{
-		public LaminaEntradaDatos (String nombre_campo, int ancho_campo) {
-			setLayout(new BorderLayout());
-			JLabel nombre = new JLabel(nombre_campo);
-			JTextField campo = new JTextField(ancho_campo);
-			nombre.setFont(fuentebotones);
-			campo.setFont(fuenteentrada);
-			add(nombre,BorderLayout.WEST);
-			add(campo, BorderLayout.EAST);			
-		}
-	}
-	
-	/*Esta nueva lamina la he tenido que crear para poder añadir una lamina de entrada de 
-	 * datos en la que salga la fecha de hoy. Luego eso deberá poderse cambiar*/
-	private class LaminaEntradaDatosFecha extends JPanel{
-		public LaminaEntradaDatosFecha () {
-			setLayout(new BorderLayout());
-			JLabel nombre = new JLabel("Fecha ");
-			String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
-			JTextField campo = new JTextField(date, 7);
-			campo.setFont(fuenteentrada);
-			nombre.setFont(fuentebotones);
-			campo.setHorizontalAlignment(JTextField.CENTER);
-			add(nombre,BorderLayout.WEST);
-			add(campo, BorderLayout.EAST);			
-		}
-	}
-	
-}
-
-class LaminaInferior extends JPanel{
-	public LaminaInferior () {
-		setBackground(new Color(200,205,182));
-	}
-}
-
-/*class LaminaIncidencias extends JPanel implements ActionListener{
-	
-	Incidencia [] misIncidencias=new Incidencia [100]; //No tengo claro porque he tenido que crear aquí la array, pero funciona
-	
-	JButton BotonNuevaIncidencia = new JButton("Crear incidencia");
-	
-	JButton BotonImprimirIncidencia = new JButton("Mostrar incidencia");
-	
-	public LaminaIncidencias () {
-		
-		add(BotonNuevaIncidencia);
-		
-		add(BotonImprimirIncidencia);
-		
-		BotonNuevaIncidencia.addActionListener(this);
-		
-		BotonImprimirIncidencia.addActionListener(this);
-		
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		
-		Object botonPulsado = e.getSource();
+	/*Esta es la lamina superior, en la que tendre los cuadros para introducir valores,
+	 * y tambien los botones de la aplicacion, que estaran a la derecha*/
+	class LaminaSuperior extends JPanel{
+		Font fuentebotones = new Font ("Serif",Font.BOLD,20);
+		Font fuenteentrada = new Font ("Serif",Font.PLAIN,18);
+		public LaminaSuperior() {			
+			/*Creacion de una lamina a la izquierda, donde el usuario podrá
+			 * introducir datos*/
+			JPanel laminaentrada = new JPanel();
+			add(laminaentrada);
 			
-		if (botonPulsado.equals(BotonNuevaIncidencia)) {
+			LaminaEntradaDatos entrada1 = new LaminaEntradaDatos("IET ",7);
+			LaminaEntradaDatosFecha entrada2 = new LaminaEntradaDatosFecha();		
+			LaminaEntradaDatos entrada3 = new LaminaEntradaDatos("Cliente ",10);
+			LaminaEntradaDatos entrada4 = new LaminaEntradaDatos("Descripción ",40);
 			
-			String cliente=JOptionPane.showInputDialog("Introduce el cliente");
+			laminaentrada.add(entrada1);
+			laminaentrada.add(entrada2);
+			laminaentrada.add(entrada3);
+			laminaentrada.add(entrada4);
 			
-			int iet=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de iet"));
+			/*Creación de los 4 botones de la app*/
+			JButton botoncrear = new JButton("Crear");
+			JButton botonbuscar = new JButton("Buscar");
+			JButton botonopciones = new JButton("Opciones");
+			JButton botonlimpiar = new JButton("Limpiar");
+			botoncrear.setFont(fuentebotones);
+			botonbuscar.setFont(fuentebotones);
+			botonopciones.setFont(fuentebotones);
+			botonlimpiar.setFont(fuentebotones);
 			
-			String descripcion=JOptionPane.showInputDialog("Introduce la descripcion de la incidencia");
-		
-			misIncidencias[Incidencia.getcontadorIncidencias()] = new Incidencia(cliente, iet, descripcion);
+			Oyente_Agregar oyente_agregar_incidencia = new Oyente_Agregar();
+			botoncrear.addActionListener(oyente_agregar_incidencia);
 			
+			/*Creacion de una lamina a la derecha, para poner los botones en ella*/
+			JPanel laminabotones = new JPanel();
+			laminabotones.setLayout(new GridLayout(1, 4));
+			add(laminabotones);
+			laminabotones.add(botoncrear);
+			laminabotones.add(botonbuscar);
+			laminabotones.add(botonopciones);
+			laminabotones.add(botonlimpiar);
 		}
 		
-		if (botonPulsado.equals(BotonImprimirIncidencia)) {
-			
-			for(int i = 0; i < misIncidencias.length; i++){
-				
-				if(i<Incidencia.getcontadorIncidencias()){
-					
-					System.out.println(misIncidencias[i].getDatosGenerales());
-					
-				}
-				
+		/*Esta lamina es para poder crear los campos más rapido en la lamina de la izquierda,
+		 * pongo titulo y cuadro para introducir los datos*/
+		private class LaminaEntradaDatos extends JPanel{
+			public LaminaEntradaDatos (String nombre_campo, int ancho_campo) {
+				setLayout(new BorderLayout());
+				JLabel nombre = new JLabel(nombre_campo);
+				JTextField campo = new JTextField(ancho_campo);
+				nombre.setFont(fuentebotones);
+				campo.setFont(fuenteentrada);
+				add(nombre,BorderLayout.WEST);
+				add(campo, BorderLayout.EAST);			
 			}
-		
 		}
-	
+		
+		/*Esta nueva lamina la he tenido que crear para poder añadir una lamina de entrada de 
+		 * datos en la que salga la fecha de hoy. Luego eso deberá poderse cambiar*/
+		private class LaminaEntradaDatosFecha extends JPanel{
+			public LaminaEntradaDatosFecha () {
+				setLayout(new BorderLayout());
+				JLabel nombre = new JLabel("Fecha ");
+				String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+				JTextField campo = new JTextField(date, 7);
+				campo.setFont(fuenteentrada);
+				nombre.setFont(fuentebotones);
+				campo.setHorizontalAlignment(JTextField.CENTER);
+				add(nombre,BorderLayout.WEST);
+				add(campo, BorderLayout.EAST);			
+			}
+		}	
 	}
-	
-}*/
+
+	/*Esta va a ser la lamina de abajo, en la que se iran mostrando y ordenando 
+	 * todas las incidencias que se vayan creando*/
+	class LaminaInferior extends JPanel{
+		public LaminaInferior () {
+			setBackground(new Color(200,205,182));		
+		}
+	}
+
+	/*Esta es la lamina clave, la lamina de cada incidencia individual, 
+	 * la que tiene que contener los objetos tipo incidencia*/
+	class LaminaIncidenciaInferior extends JPanel{
+		public LaminaIncidenciaInferior () {
+			setPreferredSize(new Dimension(1670, 30));
+		}
+	}
+}
+
